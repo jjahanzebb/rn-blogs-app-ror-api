14720d4e167c9fb38eed38f29aaf256610cf0e9b
Jahan Zeb Ahmed <jjahanzebb@gmail.com>
2023-03-14 14:32:05 +0500

    day 1.1 - basic screens setup and ROR configuration

---
+++ blog/app/api/blogAPI.ts
@@ -0,0 +1,45 @@
+const API_URL = "http://localhost:3000/api/v1";
+
+export interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
+interface NewPostPayload {
+  title: string;
+  body: string;
+}
+
+export async function getPosts() {
+  const requestInfo = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const response = await fetch(`${API_URL}/posts`, requestInfo);
+  const posts = await response.json();
+
+  console.log("Blog API");
+  console.log("Posts => ", posts);
+  return posts;
+}
+
+export async function createPost(payload: NewPostPayload) {
+  const requestInfo = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  };
+
+  const response = await fetch(`${API_URL}/posts`, requestInfo);
+  const posts = await response.json();
+
+  console.log("Blog API");
+  console.log("Posts => ", posts);
+  return posts;
+}
